a8d4854a79ad570c8a712b3c9380178d8be568c6
Sincrono <Sincrono@Formazione-06.mshome.net>
2017-11-30 15:15:52 +0100

    ciao

--- Mercury_001/src/main/java/com/mercury/controller/ServletReg.java
+++ Mercury_001/src/main/java/com/mercury/controller/ServletReg.java
@@ -1,30 +1,46 @@
 package com.mercury.controller;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-/**
- * Servlet implementation class ServletReg
- */
+import com.mercury.model.Ente;
+import com.mercury.model.dao.EnteImp;
+
 public class ServletReg extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ServletReg() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+      	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		RequestDispatcher disp;
+		 Ente ente ;
+		if(request.getParameter("form").equals("registrazioneEnte")) //campo hidden di RegistrazioneEnte.jsp
+		{	String messaggio=null;
+			EnteImp ei = new EnteImp();
+			ente=new Ente();
+			ente.setNomeEnte(request.getParameter("nomeEnte"));
+			ente.setEmailEnte(request.getParameter("emailEnte"));
+			boolean trovato = EnteImp.trovaEnte(ente);
+			if(trovato==true)
+			{	
+				messaggio = " Impossibile registrarti, il nome o l'email sono gia' presenti nel database";
+				disp=request.getRequestDispatcher("view/RegistrazioneEnte.jsp");	
+				request.setAttribute("giaEsiste", messaggio);
+				disp.forward(request, response);
+			} 
+			else 
+			{
+				messaggio= "richiesta di registrazione effettuata, attendi email di conferma"; // alert!!
+				disp=request.getRequestDispatcher("HomePage.jsp");	
+				request.setAttribute("inAttesa", messaggio);
+				disp.forward(request, response);
+			}
+		
 	}
 
 }
+
