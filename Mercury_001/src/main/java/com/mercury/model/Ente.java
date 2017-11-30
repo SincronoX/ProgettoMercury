32c7e05a0b446620e04dd535c28054ca61c70242
LucaSibilia <luca.sibilia@hotmail.it>
2017-11-30 16:43:36 +0100

    Ente

--- Mercury_001/src/main/java/com/mercury/model/Ente.java
+++ Mercury_001/src/main/java/com/mercury/model/Ente.java
@@ -1,5 +1,50 @@
 package com.mercury.model;
 
 public class Ente {
-
+	private int 	idEnte 		= 	0;
+	private String  nomeEnte 	= 	null;
+	private String  emailEnte 	= 	null;
+	private String 	pswEnte 	= 	null;
+	private int 	nBan 		= 	0;
+	private String 	status		= "attesa";
+	
+	
+	public int getIdEnte() {
+		return idEnte;
+	}
+	
+	public String getNomeEnte() {
+		return nomeEnte;
+	}
+	
+	public void setNomeEnte(String nomeEnte) {
+		this.nomeEnte = nomeEnte;
+	}
+	
+	public String getEmailEnte() {
+		return emailEnte;
+	}
+	
+	public void setEmailEnte(String emailEnte) {
+		this.emailEnte = emailEnte;
+	}
+	
+	public String getPswEnte() {
+		return pswEnte;
+	}
+	
+	public void setPswEnte(String pswEnte) {
+		this.pswEnte = pswEnte;
+	}
+	
+	public int getnBan() {
+		return nBan;
+	}
+	
+	public void setnBan(int nBan) {
+		this.nBan = nBan;
+	}
+	
 }
+
+
