4497ebb9ce4295a74f9700f3068623d146ad5737
DorianShurdhi <dorianshurdhi@hotmail.it>
2017-11-30 16:52:25 +0100

    aggiunto servletLogin

--- Mercury_001/src/main/java/com/mercury/controller/ServletLogin.java
+++ Mercury_001/src/main/java/com/mercury/controller/ServletLogin.java
@@ -1,5 +1,36 @@
 package com.mercury.controller;
 
-public class ServletLogin {
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ServletLogin extends HttpServlet {  
+	public void doPost(HttpServletRequest request, HttpServletResponse response)  
+	        throws ServletException, IOException {
+	
+	 response.setContentType("LoginEnte/jsp");  
+	    PrintWriter out = response.getWriter();  
+	          
+	    String email=request.getParameter("email");  
+	    String password=request.getParameter("password");  
+	          
+	   if(LoginDao.validate(email, password)){  
+	        RequestDispatcher rd=request.getRequestDispatcher("servletEnte");  
+	        rd.forward(request,response);  
+	    }  
+	    else{  
+	        out.print("Sorry username or password error");  
+	        RequestDispatcher rd=request.getRequestDispatcher("RegistrazioneEnte.jsp");  
+	        rd.include(request,response);  
+	    }  
+	          
+	    out.close();  
+	    }  
+	
+	
 
 }
