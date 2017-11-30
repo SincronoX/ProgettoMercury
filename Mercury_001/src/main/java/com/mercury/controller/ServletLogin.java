f4bcf40abe34ca029a447d8bc5ca0c5281f3289a
DorianShurdhi <dorianshurdhi@hotmail.it>
2017-11-30 15:46:58 +0100

    aggiunto ServletLogin

---
+++ Mercury_001/src/main/java/com/mercury/controller/ServletLogin.java
@@ -0,0 +1,5 @@
+package com.mercury.controller;
+
+public class ServletLogin {
+
+}
--- Mercury_001/src/main/java/com/mercury/model/Evento.java
+++ Mercury_001/src/main/java/com/mercury/model/Evento.java
@@ -1,5 +1,7 @@
 package com.mercury.model;
 
+import java.sql.Date;
+
 public class Evento {
 	private int idEvento;
 	private String nomeEvento;
@@ -8,6 +10,48 @@ public class Evento {
 	private Date dataInizio; 
 	private Date dataFine;
 	private boolean check;
+	public int getIdEvento() {
+		return idEvento;
+	}
+	public void setIdEvento(int idEvento) {
+		this.idEvento = idEvento;
+	}
+	public String getNomeEvento() {
+		return nomeEvento;
+	}
+	public void setNomeEvento(String nomeEvento) {
+		this.nomeEvento = nomeEvento;
+	}
+	public String getDescEvento() {
+		return descEvento;
+	}
+	public void setDescEvento(String descEvento) {
+		this.descEvento = descEvento;
+	}
+	public int getIdTipoEvento() {
+		return idTipoEvento;
+	}
+	public void setIdTipoEvento(int idTipoEvento) {
+		this.idTipoEvento = idTipoEvento;
+	}
+	public Date getDataInizio() {
+		return dataInizio;
+	}
+	public void setDataInizio(Date dataInizio) {
+		this.dataInizio = dataInizio;
+	}
+	public Date getDataFine() {
+		return dataFine;
+	}
+	public void setDataFine(Date dataFine) {
+		this.dataFine = dataFine;
+	}
+	public boolean isCheck() {
+		return check;
+	}
+	public void setCheck(boolean check) {
+		this.check = check;
+	}
 	
 	
 	
