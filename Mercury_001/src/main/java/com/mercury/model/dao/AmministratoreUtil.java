dc694c672927e699b4cc5a8a856945707b27d342
SilvioCorso <34095410+SilvioCorso@users.noreply.github.com>
2017-12-01 15:01:24 +0100

    implementate le vostre classi IMP

--- Mercury_001/src/main/java/com/mercury/model/dao/AmministratoreUtil.java
+++ Mercury_001/src/main/java/com/mercury/model/dao/AmministratoreUtil.java
@@ -1,11 +1,13 @@
 package com.mercury.model.dao;
 
+import com.mercury.model.Amministratore;
 import com.mercury.model.Ente;
 import com.mercury.model.EventoPrevisto;
 
 public interface AmministratoreUtil {
 	
 
+	public Amministratore getAdminById(int id);
 	//l'amministratore abilità l'iscrizione dell'Ente 
 	public void InvioMailAbilitaEnte(Ente en);
 	/* prende mail e nome i8nserito in fase di registrazione dall'ente
--- Mercury_001/src/main/java/com/mercury/model/dao/PrefTipoUtil.java
+++ Mercury_001/src/main/java/com/mercury/model/dao/PrefTipoUtil.java
@@ -2,4 +2,5 @@ package com.mercury.model.dao;
 
 public interface PrefTipoUtil {
 
+	public PrefTipo getPrefTipoById(int id);
 }
