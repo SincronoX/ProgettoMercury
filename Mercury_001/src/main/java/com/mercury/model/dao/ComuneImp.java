eb816a38a07ce18a8ec4c29a8a94177540a5a71d
Fabio Savelli <34094767+fabioSavelli@users.noreply.github.com>
2017-12-01 15:32:23 +0100

    eventi e comune

--- Mercury_001/src/main/java/com/mercury/model/dao/ComuneImp.java
+++ Mercury_001/src/main/java/com/mercury/model/dao/ComuneImp.java
@@ -1,10 +1,45 @@
 package com.mercury.model.dao;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.mercury.model.Comune;
+import com.mercury.model.EventoPassato;
+
 public class ComuneImp implements ComuneUtil {
 
+	Connection conn = null;
+	
 	public String getIdComune(String regione, String provincia, String comune) {
 		// TODO Auto-generated method stub
 		return "";
+	}
+
+	public Comune getComuneById(String id) throws SQLException {
+		// TODO Auto-generated method stub
+		if(conn==null) conn=DAO.getConnection();
+        Statement st = conn.createStatement();
+        
+        ResultSet rs = st.executeQuery("SELECT * FROM mercury.eventoprevisto where idEvento = "+id);
+        
+        Comune nuovo = new Comune();
+        
+        try {
+        	rs.first();
+       		
+       		String idComune =     rs.getString("idComune");
+       		String nomeComune =   rs.getString("nomeComune");
+       		
+       		nuovo.setIdComune(idComune);
+       		nuovo.setNomeComune(nomeComune);
+       		
+        }catch (Exception e) {
+			// TODO: handle exception
+        	e.printStackTrace();
+		}
+        return nuovo;
 	}
 
 }
