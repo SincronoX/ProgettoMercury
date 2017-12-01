282af308438ba140908807dca9761ac103a11fc2
DorianShurdhi <dorianshurdhi@hotmail.it>
2017-12-01 13:46:55 +0100

    ...

--- Mercury_001/src/main/java/com/mercury/model/dao/AmministratoreImp.java
+++ Mercury_001/src/main/java/com/mercury/model/dao/AmministratoreImp.java
@@ -2,9 +2,45 @@ package com.mercury.model.dao;
 
 import com.mercury.model.Ente;
 import com.mercury.model.EventoPrevisto;
+import com.mysql.jdbc.Connection;
 
 public class AmministratoreImp implements AmministratoreUtil {
 	
+Connection conn=null;
+	
+	public AmministratoreImp() {
+		super();
+		conn=DAO.getConnection();
+	}
+	
+	public int getId(String email, String password) throws SQLException{
+        if(conn==null) conn=DAO.getConnection();
+        Statement st = conn.createStatement();
+        int id=0;
+        
+        ResultSet rs = st.executeQuery("select idAdmin from amministratore where emailAdmin='"+email+"' and pswAdmin='"+password+"'");
+        
+        if(!rs.first()) {
+        	return -1;
+        }
+        id=Integer.parseInt(rs.getString("idAdmin"));
+        return id;
+    }
+	
+	public String getEmailAdmin(int idAdmin) throws SQLException{
+        if(conn==null) conn=DAO.getConnection();
+        Statement st = conn.createStatement();
+        String email = "";
+        
+        ResultSet rs = st.executeQuery("select emailAdmin from amministratore where idAdmin="+idAdmin);
+        try {
+        	rs.first();
+        	user=rs.getString("email");
+        }catch(SQLException e) {
+        	return "";
+        }
+        return email;
+    }
 	
 	public void abilitaEnte(String email, String nome) {
 		
@@ -17,5 +53,7 @@ public class AmministratoreImp implements AmministratoreUtil {
 	public void checkEvento(EventoPrevisto ep, Ente en) {
 		
 	}
+	
+	
 
 }
