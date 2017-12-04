eb427f0f1896b7ed6fc2bdea1b3ffae370c94a12
Alessio Monterosso <34095335+AlessioMonterosso@users.noreply.github.com>
2017-12-04 10:19:55 +0100

    aggiornamento

--- Mercury_001/src/main/java/com/mercury/model/dao/InvioNewsLetterMens.java
+++ Mercury_001/src/main/java/com/mercury/model/dao/InvioNewsLetterMens.java
@@ -18,7 +18,7 @@ public class InvioNewsLetterMens extends Thread implements Runnable {
 			SendMail s = new SendMail();
 			s.SendMail(3);
 			try {
-				Thread.sleep(604800);
+				Thread.sleep(2629800);
 			}
 			catch (InterruptedException e){
 				e.printStackTrace();
