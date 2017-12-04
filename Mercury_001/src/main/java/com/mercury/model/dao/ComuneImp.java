e28491629be3b945fe7d1c4c0c5f0c5c243dfac5
clelia88 <34094918+clelia88@users.noreply.github.com>
2017-12-04 17:13:21 +0100

    mod

--- Mercury_001/src/main/java/com/mercury/model/dao/ComuneImp.java
+++ Mercury_001/src/main/java/com/mercury/model/dao/ComuneImp.java
@@ -25,7 +25,7 @@ public class ComuneImp implements ComuneUtil {
 		if(conn==null) conn=DAO.getConnection();
         Statement st = conn.createStatement();
         
-        ResultSet rs = st.executeQuery("SELECT * FROM mercury.eventoprevisto where idComune = '"+id+"'");
+        ResultSet rs = st.executeQuery("SELECT * FROM mercury.eventoprevisto where idComune = '"+id+"';");
         
         Comune nuovo = new Comune();
         
