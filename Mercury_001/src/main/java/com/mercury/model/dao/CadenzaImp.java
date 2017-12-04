c2f3acb750ecc7e20dcc438c2a504a75c00af49e
SilvioCorso <34095410+SilvioCorso@users.noreply.github.com>
2017-12-04 11:47:00 +0100

    tyacci vostra

--- Mercury_001/src/main/java/com/mercury/model/dao/CadenzaImp.java
+++ Mercury_001/src/main/java/com/mercury/model/dao/CadenzaImp.java
@@ -3,47 +3,17 @@ package com.mercury.model.dao;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-<<<<<<< HEAD
 import java.sql.Statement;
 
 import com.mercury.model.Cadenza;
 
 
-=======
-
 import com.mercury.model.Cadenza;
 import com.mercury.model.TipoEvento;
->>>>>>> branch 'master' of https://github.com/SincronoX/ProgettoMercury.git
 
 public class CadenzaImp implements CadenzaUtil {
 	
-	Connection conn = null;
 	
-	public Cadenza getCadenzaById(int id) throws SQLException
-	{
-		if(conn==null) conn=DAO.getConnection();
-        Statement st = conn.createStatement();
-        
-        ResultSet rs = st.executeQuery("SELECT * FROM mercury.cadenza where idCadenza = "+id);
-        
-        Cadenza x  = new Cadenza();
-        
-        
-        try {
-        	rs.first();
-        	
-        	String frequenza = rs.getString("frequenza"); 
-        	
-        	x.setFrequenza(frequenza);
-        }
-        catch (Exception e)
-        {
-        	e.printStackTrace();
-        }
-        
-        return x;
-	
-	}
 
 	public Cadenza getCadenzaById(int id) {
 		Cadenza x= new Cadenza();
